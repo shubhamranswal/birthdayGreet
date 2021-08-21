f5e5483bec139e3adb63fedc95ceadcb78bfbd84
Shubham Singh Ranswal <shubhamranswal@gmail.com>
2021-08-21 20:37:12 +0530

    initial

---
+++ app/src/main/java/com/example/birthdaygreet/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.birthdaygreet
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
